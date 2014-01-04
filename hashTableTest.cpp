0ea734ceba33e70108eda7b2d6090966ff198b04
bornreddy <marisa.reddy@gmail.com>
2014-01-04 11:51:45 -0500

    command line filename

--- hashTableTest.cpp
+++ hashTableTest.cpp
@@ -1,8 +1,31 @@
 #include "hashTable.h"
 #include <iostream>
 
-int main() {
-  HashTable h = HashTable(10);
+int fileSize( string filename) {
+  int count = 0;
+  string line;
+  ifstream file (filename.c_str());
+  if (file.is_open())
+    {
+      while ( getline (file,line) )
+        {
+          count++;
+        }
+      file.close();
+    }
+  return count;
+}
+
+
+int main( int argc, char* argv[] ) {
+  if (argc < 2) {
+    cout << "filename expected" << endl;
+    return 0;
+  }
+  int m = fileSize(argv[1]);
+  HashTable h = HashTable(m);
+  h.insertFile(argv[1]);
+  cout << "inserted file"<< endl;
   h.insert("marisa");
   if (h.find("marisa")) {
     cout << "true" << endl;
