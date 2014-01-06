e6a8849955b78fb9f97b7762dd3b4c2e6f09f555
bornreddy <marisa.reddy@gmail.com>
2014-01-05 23:25:55 -0500

    add new test

--- hashTableTest.cpp
+++ hashTableTest.cpp
@@ -28,36 +28,33 @@ int main( int argc, char* argv[] ) {
     cout << "filename expected" << endl;
     return 0;
   }
-  int m = fileSize(argv[1]);
+  int count = 0;
+  string line;
+  ifstream file (filename.c_str());
+  if (file.is_open())
+    {
+      while ( getline (file,line) )
+        {
+          count++;
+	}
+      file.close();
+    }
+  else {
+    cout << "could not open file" << endl;
+    return 0;
+  }
+  int m = count;
   HashTable h = HashTable(m);
   h.insertFile(argv[1]);
-  cout << "inserted file"<< endl;
-  //  h.insert("marisa");
-  /* if (h.find("marisa")) {
-    cout << "true" << endl;
-    } 
-  else {
-    cout << "false" << endl;
-    } */
-  /* bool go = true;
-  while (go == true) {
-    cout << "enter word to check: " << endl;
-    string m;
-    cin << m << endl;
-    if (h.find(m)) {
-      cout << m " is in the hashTable." << endl;
-    }
-    else {
-      cout << m " is not in the hashTable." << endl;
-    }
-    
-    }*/
-
-  if (  h.find("10th") ) {
+  cout << "inserted contents of file into hashTable" << endl;
+  string target;
+  cout << "enter target word: " << endl;
+  cin >> target;
+  if (  h.find(target) ) {
     cout << "true" << endl;
   }
-
-
-
+  else {
+    cout << "could not find " + target + " in hashTable" << endl;
+  }
   return 0;
 }
